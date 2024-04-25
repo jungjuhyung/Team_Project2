07ab1ae0e16a92580c8c6044185808f251655f9a
koyechan <dpcks4759@naver.com>
2024-04-25 18:24:14 +0900

    ko

--- src/main/java/com/ict/travel/ko/controller/KoController.java
+++ src/main/java/com/ict/travel/ko/controller/KoController.java
@@ -1,5 +1,48 @@
 package com.ict.travel.ko.controller;
 
-public class KoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ict.travel.ko.dao.KoVO;
+import com.ict.travel.ko.service.KoService;
+
+@Controller
+public class KoController {
+	
+	@Autowired
+	private KoService koService;
+	
+	@RequestMapping("ko.do")
+	public ModelAndView getKo(String areacode, String contenttypeid) {
+		ModelAndView mv = new ModelAndView("ko_view/ko");
+		System.out.println("지역코드 : " + areacode);
+		System.out.println("테마종류 : " + contenttypeid);
+		
+		List<KoVO> area_list = koService.getAreaList(areacode);
+		List<KoVO> tema_list = koService.getTemaList(contenttypeid);
+		
+		System.out.println(area_list);
+		System.out.println(tema_list);
+		
+		mv.addObject("area_list", area_list);
+		mv.addObject("tema_list", tema_list);
+		
+		return mv;
+	}
+	
+	@RequestMapping("ko_detail.do")
+	public ModelAndView getKoDetail(@ModelAttribute("contentid") String contentid, 
+			@ModelAttribute("contenttypeid") String contenttypeid) {
+		ModelAndView mv = new ModelAndView("ko_view/detail");
+		System.out.println("contentid : " + contentid);
+		System.out.println("contenttypeid : " + contenttypeid);
+		return mv;
+	}
+	
+	
 }
--- src/main/webapp/WEB-INF/views/ko_view/ko.jsp
+++ src/main/webapp/WEB-INF/views/ko_view/ko.jsp
@@ -1,12 +1,60 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<style type="text/css">
+	.main_img{text-align: center;}
+	.container{display: flex;}
+	.wish li{list-style-type: none; padding-right: 20px;}
+	.wish img{width: 200px; height: 200px;}
+	.wish a{text-decoration: none;}
+</style>
+<script type="text/javascript">
+	
+</script>
 </head>
 <body>
-
+	
+	<h1>메인페이지</h1>
+	<hr>
+	<section style="margin: 0 auto; width: 1300px;">
+		<div class="main_img">
+			<img alt="" src="http://tong.visitkorea.or.kr/cms/resource/55/1842055_image2_1.jpg">
+		</div>
+		<hr>
+		<h2>지역별 장소 5개</h2>
+		<div class="wish">
+			<ul class="container">
+				<c:forEach var="k" items="${area_list}">
+					<li class="item">
+						<a href="ko_detail.do?contentid=${k.contentid}">
+							<img alt="" src="${k.firstimage}">
+							<br>${k.title}
+						</a>
+					</li>	
+				</c:forEach>
+			</ul>
+		</div>
+		<hr>
+		<h2>테마별 장소 5개</h2>
+		<div class="wish">
+			<ul class="container">
+				<c:forEach var="k" items="${tema_list}">
+					<li class="">
+						<a href="ko_detail.do?contentid=${k.contentid}&contenttypeid=${k.contenttypeid}">
+							<img alt="" src="${k.firstimage}">
+							<br>${k.title}
+						</a>
+					</li>	
+				</c:forEach>
+			</ul>
+		</div>
+	</section>
+	
+	
 </body>
 </html>
