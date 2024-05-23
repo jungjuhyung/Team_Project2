a6b1358e0c7444bba3b38268cf44163caf105c0c
wngud01 <ramyun1537@gmail.com>
2024-05-23 09:35:32 +0900

    0523_아침병합

--- src/main/java/com/ict/travel/jung/controller/GPTController.java
+++ src/main/java/com/ict/travel/jung/controller/GPTController.java
@@ -64,7 +64,7 @@ public class GPTController {
 	@RequestMapping(value = "assitest", produces="application/json; charset=utf-8")
 	@ResponseBody
 	// @RequestBody를 통해 요청의 Body부분에 있는 데이터를 받아서 사용 가능합니다.
-	public  String assitest() {
+	public String assitest() {
 		try {
 			String apiURL = "https://api.openai.com/v1/assistants";
 			String api_key = "sk-proj-yEkSRF1dONAgQbeCrVazT3BlbkFJvbZYLevgHSgz0Icexd0c";
@@ -76,54 +76,6 @@ public class GPTController {
 	                "    \"tool_resources\": {\"file_search\": {\"vector_store_ids\": [\"vs_oZkAlBMikSAYzdA5cuZWeOCM\"]}},\n" +
 	                "    \"model\": \"gpt-4-turbo\"\n" +
 	                "}";
-			System.out.println(messages);
-			// POST 요청
-			conn.setRequestMethod("POST");
-			conn.setDoOutput(true);
-			// 헤더 요청
-			conn.setRequestProperty("Content-Type", "application/json");
-			conn.setRequestProperty("Authorization", "Bearer "+api_key);
-			conn.setRequestProperty("OpenAI-Beta", "assistants=v2");
-			
-			BufferedWriter bw = 
-					new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-			bw.write(messages);
-			bw.flush();
-			int responeseCode = conn.getResponseCode();
-			System.out.println(responeseCode);
-			if(responeseCode == HttpURLConnection.HTTP_OK) {
-				BufferedReader br =
-						new BufferedReader(new InputStreamReader(conn.getInputStream()));
-				
-				String line ="";
-				StringBuffer sb2 = new StringBuffer();
-				while((line=br.readLine()) !=null) {
-					sb2.append(line);
-				}
-				String result = sb2.toString();
-				
-				return result;
-			}
-		} catch (Exception e) {
-			System.out.println("연결 실패");
-		}
-		return null;
-	}
-	@RequestMapping(value = "assitest", produces="application/json; charset=utf-8")
-	@ResponseBody
-	// @RequestBody를 통해 요청의 Body부분에 있는 데이터를 받아서 사용 가능합니다.
-	public  String assitest() {
-		try {
-			String apiURL = "https://api.openai.com/v1 /threads/thread_rhKkhMf6Np7b8e7F5kRpR0OG/messages";
-			String api_key = "sk-proj-yEkSRF1dONAgQbeCrVazT3BlbkFJvbZYLevgHSgz0Icexd0c";
-			URL url = new URL(apiURL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			String messages = "{\n" +
-					"    \"instructions\": \"You are an HR bot, and you have access to files to answer employee questions about company policies.\",\n" +
-					"    \"tools\": [{\"type\": \"file_search\"}],\n" +
-					"    \"tool_resources\": {\"file_search\": {\"vector_store_ids\": [\"vs_oZkAlBMikSAYzdA5cuZWeOCM\"]}},\n" +
-					"    \"model\": \"gpt-4-turbo\"\n" +
-					"}";
 			System.out.println(messages);
 			// POST 요청
 			conn.setRequestMethod("POST");
